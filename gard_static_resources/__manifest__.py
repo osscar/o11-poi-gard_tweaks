53d7dcb5ec0716a1fa0a917b2a457042217ddda6
squid <n.hartmann@gmail.com>
2020-03-29 20:19:56 -0400

    [addon] update/'gard_static_resources': css enhancements
    * provides basic css enhancements

---
+++ gard_static_resources/__manifest__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#    Author: squid
+#    >>> Copyleft and swindle theft <<<
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+{
+    'name': 'GARD Static Resources',
+    'summary': 'GARD static resources',
+    'descrption': """
+GARD styles for views
+==========================
+This module adds referential price rules (pricelist items)
+for price negotiation, sale type, partner specific pricing, etc.
+
+It also adds additional views and colors for enhanced management
+and viewability, including:
+
+* pricelist items on product form
+* enhanced form and tree views
+* custom fields: sale price, unit price, pricelist partner
+* colored item lines for pricelists related to partners
+* pricelists related to partner in partner form
+
+- TO DO: security groups not set
+
+""",
+    'version': '11.1.01',
+    'author': 'squid',
+    'category': 'Styles',
+    'license': 'AGPL-3',
+    'complexity': 'easy',
+    'images': [],
+    'depends': [
+        'web',
+    ],
+    'demo': [],
+    'data': [
+        'views/gard_static_resources_templates.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'test': [],
+}
