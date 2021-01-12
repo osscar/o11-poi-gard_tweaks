9b5db2f7ee00db25a8bb985826b5ba22698ab311
squid <squid@squid-8mac-2.local>
2021-01-12 11:52:18 -0400

    [addon extension] new/gard_sale_product_margin (sale_margin,product_margin)
    new addon extension for Odoo 'sale_margin' and 'product_margin' addons.
    
    * adds custom security group
    * implies custom security to fields
    * product margins report only available to custom security group

---
+++ gard_sale_product_margin/__manifest__.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Bli Bli, Ltd.
+#    Copyleft and swindle theft.
+#    Author: squid
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'GARD Sale Margin Tweaks',
+    'version': '11.01',
+    'author': 'squid',
+    'company': '',
+    'website': 'http://wwf.comcom.com',
+    'summary': 'Tweaks for Sales and Product Margins.',
+    'descrption': """
+GARD Sales and Product Margins
+==========================
+This module tweaks the viewability and security of 'product_margin'
+and 'sale_margin' addons.
+
+*
+*
+*
+
+
+""",
+    'images': [],
+    'depends': [
+        'product',
+        'sale',
+        'sale_margin',
+        'product_margin',
+    ],
+    'category': 'Sales',
+    'data': [
+        'security/gard_sale_product_margin_security.xml',
+        'security/ir.model.access.csv',
+        'views/sale_margin_view.xml',
+        'views/product_product_views.xml',
+    ],
+    'demo': [],
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
