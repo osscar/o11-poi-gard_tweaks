dda6c2603138d41b6da70dcec9e000ef4534f3b3
squid <squid@squid-8mac-2.local>
2021-03-20 11:15:14 -0400

    [addons/fix]: 'gard_x_gard' >>> fix models/init.py: comment payment_request

--- gard_x_gard/models/__init__.py
+++ gard_x_gard/models/__init__.py
@@ -6,6 +6,6 @@ from . import account_payment
 from . import account_expenses_rendition
 from . import account_invoice
 from . import account_deposit
-from . import account_payment_request
+# from . import account_payment_request
 # from . import account_cash_movement
 from . import stock_warehouse
