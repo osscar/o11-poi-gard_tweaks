e23e62643ba4f1f05f8c88105341f5c98e13a3f9
osscar <n.hartmann@gmail.com>
2021-06-28 17:04:08 -0400

    [addon/update]: 'gard_order_history' >>> added payment tree to sale order history + es_BO.po translations
    //*
       * added invoice_payment_ids tree to sale order history
       * created custom account payment tree view for order history
       * added es_BO translations

--- gard_order_history/models/sale.py
+++ gard_order_history/models/sale.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 import logging
+
 import datetime
 from odoo import models, fields, api, exceptions, _
+
+_logger = logging.getLogger(__name__)
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    invoice_ids = fields.Many2many(
-        "account.invoice",
-        string='Invoices',
-        compute="_get_invoiced",
-        readonly=True,
-        copy=False,
-        track_visibility='onchange')
+    invoice_ids = fields.Many2many('account.invoice', string='Invoices', compute='_get_invoiced',
+        copy=False, readonly=True, track_visibility='onchange')
+    invoice_payment_ids = fields.Many2many('account.payment', string='Invoice Payments', compute='_get_invoice_payments',
+        copy=False, readonly=True, track_visibility='onchange')
+
+    @api.multi
+    @api.depends('invoice_ids')
+    def _get_invoice_payments(self):
+        for order in self:
+            if order.invoice_ids:
+                order.invoice_payment_ids = order.mapped('invoice_ids').mapped('payment_ids')
+                _logger.debug('>>>>: %s', (order.invoice_payment_ids))
+
