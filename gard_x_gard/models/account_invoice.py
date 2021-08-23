705a6724c873713eab3c589072bb7400b0d77fac
osscar <n.hartmann@gmail.com>
2021-08-23 06:29:12 -0400

    Revert "[addon/update]: 'gard_x_gard' >>> allow write post"
    This reverts commit bf23df00f9d20f8088b09a4d483638b0248a07d3.

--- gard_x_gard/models/account_invoice.py
+++ gard_x_gard/models/account_invoice.py
@@ -21,13 +21,13 @@ class AccountInvoice(models.Model):
         action_validate_invoice_payment = request.params.get('method') in 'action_validate_invoice_payment'
         invoice_refund = request.params.get('method') in 'invoice_refund'
         move_reconcile = request.params.get('method') in ('assign_outstanding_credit', 'remove_move_reconcile')
-        post = request.params.get('method') in ('post')
-        cancel = request.params.get('method') in ('cancel')
-        action_draft = request.params.get('method') in ('action_draft')
         group_account_edit = self.env.user.has_group('gard_x_gard.group_account_edit')
-        allow_write = action_validate_invoice_payment or action_invoice_open or invoice_refund or move_reconcile or post or cancel or action_draft or group_account_edit
-        _logger.debug('Requested params method >>>: [%s.%s]' % (request.params.get('model'), request.params.get('method')))
-        # _logger.debug('state >>>>: %s', self.mapped('state'))
+        allow_write = action_validate_invoice_payment or action_invoice_open or invoice_refund or move_reconcile or group_account_edit
+        _logger.debug('Requested params method: [%s.%s]' % (request.params.get('model'), request.params.get('method')))
+        _logger.debug('Allow invoice_refund method: %s', invoice_refund)
+        _logger.debug('Allow move_reconcile method: %s', move_reconcile)
+        _logger.debug('Allow write: %s', allow_write)
+        _logger.debug('state >>>>: %s', self.mapped('state'))
         if any(state != 'draft' for state in set(self.mapped('state')) if allow_write == False):
             raise UserError(_('Edit allowed only in draft state. [%s.%s]' % (request.params.get('model'), request.params.get('method'))))
         else:
