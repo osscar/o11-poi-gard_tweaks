28049628f26ff9c17d6de184c4b234e57e54bf14
Osscar Charlee Papah <n.hartmann@gmail.com>
2022-04-15 17:14:13 -0400

    [FIX]: gard_x_gard >>> __init__.py

--- gard_x_gard/models/__init__.py
+++ gard_x_gard/models/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import res_company
+#from . import res_company
 from . import account
-from . import account_invoice
-from . import account_payment
+#from . import account_invoice
+#from . import account_payment
 from . import stock_picking
 # from . import procurement
 from . import stock_move
 from . import stock_warehouse
-from . import sale
+#from . import sale
 # from . import res_config_settings
