590f4baa495e64bcf5964aac88e81ed66f051e8a
ODOO <odoo@ip-172-31-64-72.us-west-2.compute.internal>
2022-08-22 19:37:38 +0000

    #46 [ENH] - gard_x_gard: added route_id propagation to sale order lines in sale order form

--- gard_x_gard/models/sale.py
+++ gard_x_gard/models/sale.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import logging
+# import logging
 
 from odoo import api, fields, models, _
 from odoo.exceptions import RedirectWarning, UserError, ValidationError
@@ -9,13 +9,27 @@ from odoo.http import request
 
 import odoo.addons.decimal_precision as dp
 
-_logger = logging.getLogger(__name__)
+# _logger = logging.getLogger(__name__)
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    team_id = fields.Many2one('crm.team', 'Sales Channel', change_default=False, default=False, track_visibility='onchange')
+    team_id = fields.Many2one('crm.team', 
+                              'Sales Channel', 
+                              change_default=False, 
+                              default=False, 
+                              track_visibility='onchange')
+    route_id = fields.Many2one('stock.location.route', 
+                               string='Propagate Route', 
+                               domain=[('sale_selectable', '=', True)])
+
+    # propagate route over sale order lines
+    @api.onchange('route_id')
+    @api.multi
+    def _propagate_route_id(self):
+        for line in self.order_line:
+            line.route_id = self.route_id
 
     # @api.depends('state')
     # def _check_team_id(self):
@@ -39,18 +53,32 @@ class SaleOrder(models.Model):
             return super().write(values)
             # _logger.info('Written values: %s', values)
 
+    
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    date_order_id = fields.Datetime('Order Date', related='order_id.date_order', store=True, readonly=True, index=True)
+    def _get_default_route_id(self):
+        domain = [('sale_selectable', '=', True)]
+        return self.env['stock.location.route'].search(domain, limit=1, order='id').id
+
+    date_order_id = fields.Datetime('Order Date', 
+                                    related='order_id.date_order', 
+                                    store=True, 
+                                    readonly=True, 
+                                    index=True)
+    route_id = fields.Many2one('stock.location.route', 
+                               string='Route', 
+                               default=_get_default_route_id,
+                               domain=[('sale_selectable', '=', True)], 
+                               ondelete='restrict')
 
     @api.multi
     def write(self, values):
         immediate_transfer_process = request.params.get('method') in 'immediate.transfer.process'
         allow_write = immediate_transfer_process
         # _logger.debug('Requested params method: [%s.%s]' % (request.params.get('model'), request.params.get('method')))
-        # _logger.debug('state >>>>: %s', self.mapped('state'))
+        # _logger.debug('state >>>: %s', self.mapped('state'))
         # _logger.info('self: %s', self)
         if any(state != 'draft' for state in set(self.mapped('state')) if not allow_write):
             raise UserError(_('Edit allowed only in draft state. [%s.%s]' % (request.params.get('model'), request.params.get('method'))))
