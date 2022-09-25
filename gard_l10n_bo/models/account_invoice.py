c742015dde82be917c629f8c4e6b7950cb0d73a6
ODOO <odoo@ip-172-31-64-72.us-west-2.compute.internal>
2022-09-25 23:57:44 +0000

    #57 - [ENH] gard_l10n_bo: added dosification accounting sale journal set in subsidiary (sucursal) warehouse to avoid wrong journal from being selected as default journal in sale order customer invoice; added es_BO translation

---
+++ gard_l10n_bo/models/account_invoice.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# import logging
+
+from odoo import models, fields, api, _
+
+# from odoo.exceptions import ValidationError
+
+# _logger = logging.getLogger(__name__)
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.one
+    @api.depends('state')
+    def _get_warehouse(self):
+        res = super(AccountInvoice, self)._get_warehouse()
+        if self.cc_dos and self.type == 'out_invoice':
+            # set appropriate sale journal based on cc_dos.warehouse_id.journal_id
+            self.write({'journal_id': self.cc_dos.warehouse_id.journal_id.id})
+        return res
+
+    @api.onchange('cc_dos')
+    @api.one
+    def _onchange_cc_dos(self):
+        self.ensure_one()
+        if self.cc_dos and self.type == 'out_invoice':
+            # onchange cc_dos set cc_dos sale journal
+            self.journal_id = self.cc_dos.warehouse_id.journal_id.id
