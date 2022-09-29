a310c7968e1a443fb6391508b011ebb7e8488a74
ODOO <odoo@ip-172-31-64-72.us-west-2.compute.internal>
2022-09-29 23:25:23 +0000

    #55 - [BUG] gard_productd_price: fixed base_import do method not dry running on dryrun

--- gard_product_price/models/base_import.py
+++ gard_product_price/models/base_import.py
@@ -21,35 +21,36 @@ class Import(models.TransientModel):
     # triggered by depends fields
     @api.multi
     def do(self, fields, options, dryrun=False):
+        self.ensure_one()
         # _logger.debug('do call self >>>: %s', self)
         # _logger.debug('do call res_model >>>: %s', self.res_model)
         # _logger.debug('do call fields >>>: %s', fields)
         # _logger.debug('do call options >>>: %s', options)
-        res = super(Import, self).do(fields, options, dryrun=False)        
-        res_model = self.res_model
-        # this list unfortunately needs to be updated manually 
-        # when updating depends on pl_item _calc_price_unit method
-        pl_item_comp_fields = [
-            'applied_on',
-            'product_tmpl_id',
-            'product_id',
-            'base',
-            'base_pricelist_id',
-            'compute_price',
-            'percent_price',
-            'min_quantity',
-            'date_update',
-            'uom_pack_id'
-        ]
-        if res_model == 'product.pricelist.item' and any(fields in pl_item_comp_fields for fields in fields):
-            data, import_fields = self._convert_import_data(fields, options)
-            for line in data:
-                recompute_items = []
-                product = False
-                extid_2_id = self.env.ref(line[0]).ids
-                # _logger.debug('do call line extid_2_id >>>: %s', extid_2_id)
-                item_ids = self.env[res_model].search([('id', '=', extid_2_id)])
-                if dryrun == False:
+        res = super(Import, self).do(fields, options, dryrun=False)      
+        if dryrun == False:
+            res_model = self.res_model
+            # this list unfortunately needs to be updated manually 
+            # when updating depends on pl_item _calc_price_unit method
+            pl_item_comp_fields = [
+                'applied_on',
+                'product_tmpl_id',
+                'product_id',
+                'base',
+                'base_pricelist_id',
+                'compute_price',
+                'percent_price',
+                'min_quantity',
+                'date_update',
+                'uom_pack_id'
+            ]
+            if res_model == 'product.pricelist.item' and any(fields in pl_item_comp_fields for fields in fields):
+                data, import_fields = self._convert_import_data(fields, options)
+                for line in data:
+                    recompute_items = []
+                    product = False
+                    extid_2_id = self.env.ref(line[0]).ids
+                    # _logger.debug('do call line extid_2_id >>>: %s', extid_2_id)
+                    item_ids = self.env[res_model].search([('id', '=', extid_2_id)])
                     for item in item_ids:
                         # _logger.debug('for item call item >>>: %s', item)
                         if item.applied_on == '1_product':
@@ -64,4 +65,11 @@ class Import(models.TransientModel):
                             for r_item in recompute_items:
                                 # _logger.debug('write call r_item >>>: %s', r_item)
                                 r_item.write({'date_update': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+        try:
+            if dryrun:
+                self._cr.execute('ROLLBACK TO SAVEPOINT import')
+            else:
+                self._cr.execute('RELEASE SAVEPOINT import')
+        except psycopg2.InternalError:
+            pass
         return res
