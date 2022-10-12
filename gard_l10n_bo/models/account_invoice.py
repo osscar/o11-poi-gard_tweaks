3c88471656d67111808f9ca324b733559ccdeed9
osscar <n.hartmann@gmail.com>
2022-10-12 15:14:40 +0000

    #62 [UPD] - gard_l10n_bo: fixed singleton error on refund

--- gard_l10n_bo/models/account_invoice.py
+++ gard_l10n_bo/models/account_invoice.py
@@ -12,18 +12,20 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     # @api.one
+    @api.multi
     @api.depends("state")
     def _get_warehouse(self):
         res = super(AccountInvoice, self)._get_warehouse()
-        if self.cc_dos and self.type == "out_invoice":
-            if self.cc_dos and not self.cc_dos.warehouse_id.journal_id:
-                raise ValidationError(
-                    _("A journal must be set for the dosification branch.")
-                )
-            else:
-                # set appropriate sale journal based on cc_dos.warehouse_id.journal_id
-                self.journal_id = self.cc_dos.journal_id
-                # self.write({"journal_id": self.cc_dos.journal_id.id})
+        for invoice in self:
+            if invoice.cc_dos and invoice.type == "out_invoice":
+                if invoice.cc_dos and not invoice.cc_dos.warehouse_id.journal_id:
+                    raise ValidationError(
+                        _("A journal must be set for the dosification branch.")
+                    )
+                else:
+                    # set appropriate sale journal based on cc_dos.warehouse_id.journal_id
+                    invoice.journal_id = invoice.cc_dos.journal_id
+                    # invoice.write({"journal_id": invoice.cc_dos.journal_id.id})
         return res
 
     @api.onchange("cc_dos")
