31f31d9f94eac9ea21c0b72fb014765249e2fd17
osscar <n.hartmann@gmail.com>
2022-12-15 14:46:12 +0000

    #82 [UPD] - gard_xanalytic, gard_x_propagate: moved purchase anlytic base fields to gard_x_analytic from gard_x_propagate; upd es_BO translations

--- gard_x_analytic/models/__init__.py
+++ gard_x_analytic/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import stock_landed_cost
 from . import account_analytic_line
+from . import purchase
+from . import stock_landed_cost
---
+++ gard_x_analytic/models/purchase.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# import logging
+
+from odoo import api, fields, models, _
+
+# _logger = logging.getLogger(__name__)
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    account_analytic_id = fields.Many2one(
+        "account.analytic.account", string="Analytic Account"
+    )
