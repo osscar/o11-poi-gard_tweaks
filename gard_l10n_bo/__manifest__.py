0d95ddc19a442d1d65525fb8756f2ea7b0591f86
osscar <n.hartmann@gmail.com>
2023-05-22 22:21:31 +0000

    #100 [PRE] - pre-upd gard_l10_bo, gard_x_gard: adapt siat_unidad_medida reports pre-merge commit

--- gard_l10n_bo/__manifest__.py
+++ gard_l10n_bo/__manifest__.py
@@ -38,6 +38,10 @@ by GARD based on POIESIS\' customizations
     ],
     # 'demo': [],
     "data": [
+        'report/basic_invoice.xml',
+        'report/invoice_base.xml',
+        'report/siat_invoice.xml',
+
         'views/dosif_view.xml',
         'views/product_uom_view.xml',
         'views/stock_warehouse_view.xml',
