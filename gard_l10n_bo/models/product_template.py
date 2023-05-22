22af02e3bdb530d34f972f720c37ed00a08dad70
osscar <n.hartmann@gmail.com>
2023-05-22 14:10:20 +0000

    #100 [PRE] - pre-upd gard_l10_bo, gard_x_gard: adapt siat_unidad_medida pre-merge commit

---
+++ gard_l10n_bo/models/product_template.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# import logging
+
+from odoo import api, fields, tools, models, _
+from odoo.exceptions import UserError
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    siat_unidad_medida_id = fields.Many2one(
+        "siat.unidad_medida",
+        string="UdM SIAT",
+        compute="_get_siat_unidad_medida",
+        store="True",
+        help="La Unidad de medida SIAT por defecto con el que se enviará la venta de este Producto al SIN.",
+    )
+
+    @api.multi
+    @api.depends("uom_id", "uom_id.siat_unidad_medida_id")
+    def _get_siat_unidad_medida(self):
+        siat_uom_id = False
+        for product in self:
+            if product.uom_id and product.uom_id.siat_unidad_medida_id:
+                siat_uom_id = product.uom_id.siat_unidad_medida_id
+            product.siat_unidad_medida_id = siat_uom_id
+        return True
