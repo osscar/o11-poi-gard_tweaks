ce22d122120e38e5ee933a292c9992e21080f446
osscar <n.hartmann@gmail.com>
2023-06-03 22:22:33 +0000

    #102 [PRE] - pre-merge commit [3] > gard_x_gard: >> sale.order form lines domain product/uom >>> added product_uom_ids computed field for product_uom domain >> + TO DO: kanban order lines

--- gard_product_price/models/sale.py
+++ gard_product_price/models/sale.py
@@ -14,23 +14,13 @@ class SaleOrderLine(models.Model):
     def _get_default_pricelist_id(self):
         # _logger.debug('_g_def_p call _context >>>: %s', self._context)
         # _logger.debug('_g_def_p call _context active_ids >>>: %s', self._context.get('params').get('id'))
-        _get_order_id = self._context.get('params', {}).get('id')
+        _get_order_id = self._context.get("params", {}).get("id")
         if _get_order_id:
-            order_id = self.env['sale.order'].search([('id', '=', _get_order_id)])
+            order_id = self.env["sale.order"].search([("id", "=", _get_order_id)])
             return order_id.pricelist_id
 
-    @api.multi
-    def _get_count_active_pricelist_items(self):
-        for order_line in self:
-            product_id = order_line.product_id
-            order_line.pricelist_item_ids = product_id.pricelist_item_ids.filtered(
-                lambda item: item.active_pricelist == True
-            )
-            order_line.pricelist_item_count = len(order_line.pricelist_item_ids)
-
     pricelist_item_count = fields.Integer(
-        "Pricelist Item Count", 
-        compute="_get_count_active_pricelist_items"
+        "Pricelist Item Count", compute="_get_count_active_pricelist_items"
     )
     # added pricelist_id field to segment
     # the price_unit computation per order_line
@@ -43,6 +33,24 @@ class SaleOrderLine(models.Model):
         # states={"draft": [("readonly", False)], "sent": [("readonly", False)]},
         help="Pricelist for current sales order line.",
     )
+    product_uom_ids = fields.Many2many(
+        "product.uom", "Product UoM", compute="_get_product_uom_ids"
+    )
+
+    @api.multi
+    def _get_count_active_pricelist_items(self):
+        for order_line in self:
+            product_id = order_line.product_id
+            order_line.pricelist_item_ids = product_id.pricelist_item_ids.filtered(
+                lambda item: item.active_pricelist == True
+            )
+            order_line.pricelist_item_count = len(order_line.pricelist_item_ids)
+
+    @api.multi
+    def _get_product_uom_ids(self):
+        for order_line in self:
+            product = order_line.product_id
+            order_line["product_uom_ids"] = product.uom_id + product.uom_pack_id
 
     @api.multi
     def _get_display_price(self, product):
@@ -50,13 +58,35 @@ class SaleOrderLine(models.Model):
         # _logger.debug('_gdp call product >>>: %s', product.default_code)
         # mod'd method to work on order_line rather than the order
         if self.pricelist_id:
-            if self.pricelist_id.discount_policy == 'with_discount':
+            if self.pricelist_id.discount_policy == "with_discount":
                 return product.with_context(pricelist=self.pricelist_id.id).price
-            product_context = dict(self.env.context, partner_id=self.order_id.partner_id.id, date=self.order_id.date_order, uom=self.product_uom.id)
-            final_price, rule_id = self.pricelist_id.with_context(product_context).get_product_price_rule(self.product_id, self.product_uom_qty or 1.0, self.order_id.partner_id)
-            base_price, currency_id = self.with_context(product_context)._get_real_price_currency(product, rule_id, self.product_uom_qty, self.product_uom, self.pricelist_id.id)
+            product_context = dict(
+                self.env.context,
+                partner_id=self.order_id.partner_id.id,
+                date=self.order_id.date_order,
+                uom=self.product_uom.id,
+            )
+            final_price, rule_id = self.pricelist_id.with_context(
+                product_context
+            ).get_product_price_rule(
+                self.product_id, self.product_uom_qty or 1.0, self.order_id.partner_id
+            )
+            base_price, currency_id = self.with_context(
+                product_context
+            )._get_real_price_currency(
+                product,
+                rule_id,
+                self.product_uom_qty,
+                self.product_uom,
+                self.pricelist_id.id,
+            )
             if currency_id != self.order_id.pricelist_id.currency_id.id:
-                base_price = self.env['res.currency'].browse(currency_id).with_context(product_context).compute(base_price, self.order_id.pricelist_id.currency_id)
+                base_price = (
+                    self.env["res.currency"]
+                    .browse(currency_id)
+                    .with_context(product_context)
+                    .compute(base_price, self.order_id.pricelist_id.currency_id)
+                )
             # force final_price to avoid min_quantity restrictions on super
             return final_price
         # fall back on super if no pricelist_id is set on order_line
@@ -64,7 +94,7 @@ class SaleOrderLine(models.Model):
             return res
 
     # recompute price_unit  onchange pricelist_id
-    @api.onchange('pricelist_id')
+    @api.onchange("pricelist_id")
     @api.multi
     def _onchange_pricelist_id(self):
         # _logger.debug('_pic call self >>>: %s', self)
@@ -73,14 +103,21 @@ class SaleOrderLine(models.Model):
         product = self.product_id.with_context(
             lang=self.order_id.partner_id.lang,
             partner=self.order_id.partner_id.id,
-            quantity=vals.get('product_uom_qty') or self.product_uom_qty,
+            quantity=vals.get("product_uom_qty") or self.product_uom_qty,
             date=self.order_id.date_order,
             pricelist=self.order_id.pricelist_id.id,
-            uom=self.product_uom.id
+            uom=self.product_uom.id,
         )
         self._compute_tax_id()
         if self.pricelist_id:
-            vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
+            vals["price_unit"] = self.env[
+                "account.tax"
+            ]._fix_tax_included_price_company(
+                self._get_display_price(product),
+                product.taxes_id,
+                self.tax_id,
+                self.company_id,
+            )
         self.update(vals)
 
     @api.multi
