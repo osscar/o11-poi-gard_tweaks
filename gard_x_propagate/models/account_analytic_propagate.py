58a04f3fb1685e67d1b706507d89f54526167a49
osscar <n.hartmann@gmail.com>
2023-06-17 04:02:50 +0000

    #104 [PRE] - pre merge commit > UPD: gard_x_propagate >> revamped analytic account propagator >>> adapting new logic >> needs merge conflict revision with last merged commit >>> 17cd56a89d025780df2b560b8c7f7a9b963bc62f

---
+++ gard_x_propagate/models/account_analytic_propagate.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# import logging
+
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
+
+# _logger = logging.getLogger(__name__)
+
+
+class AccountAnalyticPropagate(models.Model):
+    _inherit = "account.analytic.propagate"
+
+    
+
+    def button_order_line_unlink(self):
+        if self.state != "draft":
+            raise ValidationError(
+                ("Cannot delete lines if order is not in draft state.")
+            )
+        for line in self.order_line:
+            line.unlink()
+        return True
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.one
+    def button_propagate_route(self):
+        if self.order_id.state != "draft":
+            raise ValidationError(("Cannot propagate if order is not in draft state."))
+        route_id = self.route_id
+        for line in self.order_id.order_line:
+            line["route_id"] = route_id
+        return True
+
+    @api.one
+    def button_propagate_pricelist(self):
+        if self.order_id.state != "draft":
+            raise ValidationError(("Cannot propagate if order is not in draft state."))
+        pricelist_id = self.pricelist_id
+        for line in self.order_id.order_line.filtered(lambda l: self.id != l.id):
+            line["pricelist_id"] = pricelist_id
+        return True
