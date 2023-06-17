018a10515639de45a1c11b5c575ea9c21f29588b
osscar <n.hartmann@gmail.com>
2023-06-17 23:42:34 +0000

    #104 [PRE] - pre merge commit > UPD: gard_x_propagate >> auto add order parent account vals inheriting write method >> needs merge conflict revision with last merged commit >>> 17cd56a89d025780df2b560b8c7f7a9b963bc62f

--- gard_x_propagate/models/account_analytic_propagate.py
+++ gard_x_propagate/models/account_analytic_propagate.py
@@ -3,51 +3,58 @@
 
 # import logging
 
-from odoo import api, models, _
-from odoo.exceptions import ValidationError
+from odoo import api, fields, models, _
+
+# from odoo.exceptions import ValidationError
 
 # _logger = logging.getLogger(__name__)
 
 
 class AccountAnalyticPropagateGroup(models.Model):
-    _inherit = "account.analytic.propagate.group"
+    name = "account.analytic.propagate.group"
 
     name = fields.Char("Name", required=True)
     type = fields.Selection(
         [
             ("purchase", "Purchase Order"),
-            # ("sale", "Sale Order"),
+            ("sale", "Sale Order"),
         ],
         string="Type",
         required=True,
         help="Select order type for group.",
     )
-    account_analytic_default_ids = fields.One2many(
-        "account.analytic.propagate.group.account",
-        "group_id",
-        string="Analytic Accounts",
-        help="Default analytic account details.",
-    )
-    active = fields.Boolean(string="Active", default=True)
-
-
-class AccountAnalyticPropagateGroupAccount(models.Model):
-    _inherit = "account.analytic.propagate.group.account"
-
-    name = fields.Char(
-        "Name",
-        required=True,
-    )
-    code = fields.Char(
-        "Code",
-        size=10,
-        required=True,
-    )
     parent_id = fields.Many2one(
         "account.analytic.account",
         string="Parent Analytic Account",
-        help="Parent analytic account for group.",
+        help="Select parnet analytic account for the order parent analytic account. eg. Purchases / Imports for PO00034",
     )
+    account_value_ids = fields.One2many(
+        "account.analytic.propagate.group.account",
+        "group_id",
+        string="Account Values",
+        help="Default analytic account values.",
+    )
+    active = fields.Boolean(string="Active", default=True)
+
+    def write(vals):
+        # create default order parent analytic account;
+        # process is automated and fields will have readonly
+        # in the form view if is_parent is true
+        res = super().write(vals)
+        acc_parent_vals = {
+            "group_id": self.group_id.id,
+            "name": "AB#####",
+            "code": "AB#####",
+            "is_parent": True,
+        }
+        self.account_value_ids.create(acc_parent_vals)
+        
+        return res
+
+
+class AccountAnalyticPropagateGroupAccount(models.Model):
+    name = "account.analytic.propagate.group.account"
+
     group_id = fields.Many2one(
         "account.analytic.propagate.group",
         string="Group",
@@ -55,12 +62,25 @@ class AccountAnalyticPropagateGroupAccount(models.Model):
         ondelete="cascade",
         help="Propagate group related to this account.",
     )
+    name = fields.Char(
+        "Name",
+    )
+    code = fields.Char(
+        "Code",
+        size=10,
+    )
+    is_parent = fields.Boolean(
+        string="Order Parent",
+        default=False,
+        readonly=True,
+        help="Parent account for order.",
+    )
 
     @api.multi
-    @api.depends('name', 'code')
+    @api.depends("name", "code")
     def name_get(self):
         res = []
         for record in self:
-            name = parent_id + ': ' + record.name + record.code
+            name = group_id.parent_id.name + "/ " + record.name + record.code
             res.append((record.id, name))
         return res
