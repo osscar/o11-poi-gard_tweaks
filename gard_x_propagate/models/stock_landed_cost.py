989bb0c861596261b34f4c6da603ad1fec28dccb
osscar <n.hartmann@gmail.com>
2023-06-21 23:28:57 +0000

    #104 [PRE] - pre merge commit > UPD: gard_x_gard/propagate >> propagate: adapt stock_landed cost buttons >> gard: stock_landed cost/line object _order >> needs merge conflict revision with last merged commit >>> 17cd56a89d025780df2b560b8c7f7a9b963bc62f

--- gard_x_propagate/models/stock_landed_cost.py
+++ gard_x_propagate/models/stock_landed_cost.py
@@ -3,7 +3,8 @@
 
 # import logging
 
-from odoo import api, fields, models, _
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
 
 # _logger = logging.getLogger(__name__)
 
@@ -11,31 +12,52 @@ from odoo import api, fields, models, _
 class LandedCost(models.Model):
     _inherit = "stock.landed.cost"
 
+    def _check_state(self):
+        verr_msg = self._context.get("verr_msg")
+        # check state
+        if self.state != "draft":
+            raise ValidationError(("%s") % verr_msg)
+
     @api.one
     def button_create_cost_line(self):
+        # check state
+        verr_msg = "Cannot create cost lines if form is not in draft state."
+        self.with_context(verr_msg=verr_msg)._check_state()
+
         anl_lines = self.account_analytic_id.line_ids
+        if not anl_lines:
+            raise ValidationError(
+                ("The analytic account has no analytic lines to propagate.")
+            )
+
         for line in anl_lines:
-            # _logger.debug('bclc vals >>>: %s', line)
+            # set default product if no product
+            # is set on landed cost line
+            product_id = self.env.ref("gard_x_propagate.product_slc_default")
             if line.product_id:
-                # _logger.debug('bclc if product_id vals >>>: %s', line.product_id)
                 product_id = line.product_id
-            else:
-                # _logger.debug('bclc else product_id vals >>>: %s', line.product_id)
-                product_id = self.env.ref('gard_x_propagate.product_slc_default')
+
+            # price_unit value needs to be inverted
+            # to match landed cost value logic
             vals = {
                 'cost_id': self.id,
                 'account_analytic_line_id': line.id,
-                'product_id': product_id.id,
-                'split_method': product_id.split_method,
+                'product_id': getattr(product_id, "id"),
+                'split_method': getattr(product_id, "split_method"),
                 'price_unit': line.amount * -1,
             }
             # _logger.debug('bclc vals >>>: %s', vals)
 
             new_lines = self.cost_lines.create(vals)
             new_lines.onchange_account_analytic_line_id()
+
         return True
 
     def button_unlink_cost_line(self):
+        # check state
+        verr_msg = "Cannot delete a cost line if form is not in draft state."
+        self.with_context(verr_msg=verr_msg)._check_state()
+
         for line in self.cost_lines:
             line.unlink()
         return True
@@ -44,33 +66,45 @@ class LandedCost(models.Model):
 class LandedCostLine(models.Model):
     _inherit = "stock.landed.cost.lines"
 
+    def _get_price_unit(self):
+        price_unit = self.account_analytic_line_id.amount
+
+        # convert currency if needed
+        if (
+            self.cost_id.account_journal_id.currency_id
+            != self.account_analytic_line_id.currency_id
+        ):
+            price_unit = self.account_analytic_line_id.currency_id.compute(
+                self.account_analytic_line_id.amount,
+                self.cost_id.account_journal_id.currency_id,
+            )
+
+        # invert value to adapt to
+        # cost line logic
+        price_unit = -1 * price_unit
+
+        return price_unit
+
     @api.onchange("product_id")
     def onchange_product_id(self):
-        res = super(LandedCostLine, self).onchange_product_id()
+        res = super().onchange_product_id()
+
+        if self.product_id:
+            self.split_method = self.product_id.split_method
+
         if self.account_analytic_line_id:
-            # convert currency if needed
-            if (
-                self.cost_id.account_journal_id.currency_id
-                != self.account_analytic_line_id.currency_id
-            ):
-                price_unit = self.account_analytic_line_id.currency_id.compute(
-                    self.account_analytic_line_id.amount,
-                    self.cost_id.account_journal_id.currency_id,
-                )
-            else:
-                price_unit = self.account_analytic_line_id.amount
-            self.price_unit = -1 * price_unit
+            # get unit price
+            self.price_unit = self._get_price_unit()
+
         return res
 
     @api.onchange("account_analytic_line_id")
     def onchange_account_analytic_line_id(self):
-        if self.product_id:
-            # _logger.debug('bclc if product_id vals >>>: %s', self.product_id)
-            product_id = self.product_id
         # use default product if analytic line doe not have aproduct_id assigned
-        else:
-            # _logger.debug('bclc else product_id vals >>>: %s', self.product_id)
-            product_id = self.env.ref('gard_x_propagate.product_slc_default')
+        product_id = self.env.ref("gard_x_propagate.product_slc_default")
+        if self.product_id:
+            product_id = self.product_id
+
         # write values to cost line fields
         self.product_id = product_id
         self.name = (
@@ -81,15 +115,5 @@ class LandedCostLine(models.Model):
         self.account_id = self.account_analytic_line_id.general_account_id
         self.analytic_account_id = self.account_analytic_line_id.account_id
         self.analytic_tag_ids = self.account_analytic_line_id.tag_ids
-        # convert currency if needed
-        if (
-            self.cost_id.account_journal_id.currency_id
-            != self.account_analytic_line_id.currency_id
-        ):
-            price_unit = self.account_analytic_line_id.currency_id.compute(
-                self.account_analytic_line_id.amount,
-                self.cost_id.account_journal_id.currency_id,
-            )
-        else:
-            price_unit = self.account_analytic_line_id.amount
-        self.price_unit = price_unit * -1
+        # get unit price
+        self.price_unit = self._get_price_unit()
