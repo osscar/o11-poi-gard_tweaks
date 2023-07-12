4e8918cb0099cf97ddccf3615494fd10abea2f58
osscar <n.hartmann@gmail.com>
2023-07-12 23:10:16 +0000

    #104 [UPD]  - gard_x_analytic: - add sql_constraint uniq_code   - many TO DO's

--- gard_x_analytic/models/account_analytic_account.py
+++ gard_x_analytic/models/account_analytic_account.py
@@ -6,20 +6,9 @@ from odoo import api, models, _
 class AccountAnalyticAccount(models.Model):
     _inherit = "account.analytic.account"
 
-    # _sql_constraints = [
-    #     ('unique_code', 'UNIQUE (code)', _('The analytic account code must be unique.'))
-    # ]
-
-    @api.multi
-    @api.constrains("firstname", "lastname")
-    def _check_name(self):
-        """Ensure at least one name is set."""
-        for record in self:
-            if all((
-                record.type == 'contact' or record.is_company,
-                not (record.firstname or record.lastname)
-            )):
-                raise exceptions.EmptyNamesError(record)
+    _sql_constraints = [
+        ('uniq_code', 'UNIQUE (code)', _('The analytic account code must be unique.'))
+    ]
 
     @api.onchange("parent_id")
     def _onchange_parent_id(self):
